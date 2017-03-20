c891bd1cc21b070aac3d9877f7b38f60d1a7dda1
Taylor Long <karthias@gmail.com>
2017-03-19 18:35:02 -0700

    Base of decoder started
    Began the basic output to video file. Will need to know the format of the incoming encoded video to construct loop to decode and write each frame.

--- decodeVideo.m
+++ decodeVideo.m
@@ -1,4 +1,24 @@
 function decodeVideo( encodedFrames )
     %Run Inverse DCT on each frame and construct video file
+    %This is just a skeleton of the process, will need correct syntax for
+    %indexing through every frame of the encoded file
     
+    %Assuming the frame has been reconstructed i.e. not VLC needing motion
+    %vectors for reconstruction etc.
+    
+    %IDCT
+    decodedFrame = blkproc(encodedFrames,[8,8],'idct2');
+    
+    %create video writer object
+    %lossless compression motion jpeg 2000 format
+    decodedVideo = VideoWriter('decoded.mj2','Archival');
+    
+    %open video for writing
+    open(decodedVideo);
+    
+    %write frame to video file
+    writeVideo(decodedVideo,decodedFrame);
+    
+    %close the video file
+    close(decodedVideo);
 end
